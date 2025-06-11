9a4c8047f2fbb89307f6ecd276c2c9d8cf16972a
asiradnan <asiradnan23@gmail.com>
2025-06-11 15:57:34 +0600

    added req

--- SocialBackend/settings.py
+++ SocialBackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cinnj(5%#i0&zguf2s#z0isc=4ad1j11nwm$_=1!9nkm8d&$@='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
