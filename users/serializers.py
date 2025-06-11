8fbe718f9dd66c6dda4187b65291dff56c03cea9
asiradnan <asiradnan23@gmail.com>
2025-06-11 18:36:46 +0600

    custom user manager

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,3 @@
-# Alternative serializer - completely removes username from registration
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
