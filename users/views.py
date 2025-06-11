cbb1ffa044e5a491601ea1eb6637c63554c7b965
asiradnan <asiradnan23@gmail.com>
2025-06-11 17:03:27 +0600

    get req temporarily added for user

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from .models import CustomUser
 
 class RegisterView(APIView):
     def post(self, request):
@@ -10,3 +11,8 @@ class RegisterView(APIView):
             serializer.save()
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        users = CustomUser.objects.all()
+        serializer = RegisterSerializer(users, many=True)
+        return Response(serializer.data)
