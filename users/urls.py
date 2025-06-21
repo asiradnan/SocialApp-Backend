ad4f733467920862719d91f756c1b42f0eff00da
asiradnan <asiradnan23@gmail.com>
2025-06-21 12:14:30 +0600

    password update

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('me/', UserView.as_view(), name='me'),
-    path('password', PasswordView.as_view(), name='password'),
+    path('password/', PasswordView.as_view(), name='password'),
 ]
