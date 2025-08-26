02a8a86d39de3c65b357d43fadd3e519219c4b2f
asiradnan <asiradnan23@gmail.com>
2025-08-26 12:48:24 +0600

    admin site has other models

--- feed/admin.py
+++ feed/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Post, Poll
+from .models import Post, Poll, Comment, PostReaction, PollVote
 
 admin.site.register(Post)
 admin.site.register(Poll)
+admin.site.register(Comment)
+admin.site.register(PostReaction)
+admin.site.register(PollVote)
